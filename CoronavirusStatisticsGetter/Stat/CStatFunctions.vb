c4477e9bc09c9bfcbaceafa12d47104d6f39e0ac
ilja89 <52709138+ilja89@users.noreply.github.com>
2022-03-17 23:07:10 +0200

    Added CStatFunctions

---
+++ CoronavirusStatisticsGetter/Stat/CStatFunctions.vb
@@ -0,0 +1,30 @@
+﻿Public Class CStatFunctions
+    Public Function Forecast(list As CStatList, fieldName As String, Optional sampleSize As Integer = 7, Optional outPointsNumber As Integer = 7) As CStatList
+        If (list.Count >= 2) Then
+            If (sampleSize > list.Count) Then
+                sampleSize = list.Count
+            ElseIf (sampleSize <= 0) Then
+                sampleSize = 2
+            End If
+            If (outPointsNumber <= 0) Then
+                outPointsNumber = 1
+            End If
+            Dim temp As Integer = 0
+            Dim avgPoint As Double
+            Dim diff As Double
+            For i As Integer = 1 To sampleSize
+                temp += list.GetField(list.Count - i, fieldName)
+            Next
+            avgPoint = CDbl(temp) / sampleSize
+            diff = list.GetField(list.Count - 1, fieldName) - avgPoint
+
+            For i As Integer = 0 To outPointsNumber
+
+            Next
+
+            Return list
+        Else
+            Return Nothing
+        End If
+    End Function
+End Class
